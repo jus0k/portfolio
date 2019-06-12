5661e8681fee8b1a3ab04275663a98804dee767b
jus0k <jutjd115@gmail.com>
2019-06-12 15:24:31 +0900

    test loading

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,9 @@
 import { menuBtn, showMenu } from './menu';
-import { showContents } from './scroll'
+import { showContents } from './scroll';
+import { showScreen } from './loading';
 
 const init = () => {
+  window.onload = showScreen();
   menuBtn.addEventListener('click', showMenu);
   window.addEventListener('scroll', showContents);
 }
---
+++ src/script/loading.js
@@ -0,0 +1,11 @@
+const loading = document.querySelector('.section-loading');
+
+const showScreen = () => {
+  loading.classList.add('hide');
+}
+
+window.onload = function () {
+  
+}
+
+export { showScreen };
